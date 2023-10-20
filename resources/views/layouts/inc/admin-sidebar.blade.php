f817f0000e90a3467eeafacf97c9f1f3bc9d441b
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2023-10-20 21:00:13 +0530

    Side Bar Completed

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -3,35 +3,35 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="{{url('admin/dashboard')}}">
+                <a class="nav-link {{Request::is('admin/dashboard')?"active":''}}" href="{{url('admin/dashboard')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
                 <div class="sb-sidenav-menu-heading">Interface</div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                <a class="nav-link  {{Request::is('admin/add-category') || Request::is('admin/category') ? 'collapse':'collapsed'}}" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                     Category
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="{{Request::is('admin/add-category') || Request::is('admin/category') ? 'show':'collapse'}}" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{url('admin/add-category')}}">Add Category</a>
-                        <a class="nav-link" href="{{url('admin/category')}}">View Category</a>
+                        <a class="nav-link {{Request::is('admin/add-category')?'active':''}}" href="{{url('admin/add-category')}}">Add Category</a>
+                        <a class="nav-link {{Request::is('admin/category')?'active':''}}" href="{{url('admin/category')}}">View Category</a>
                     </nav>
                 </div>
 
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts" aria-expanded="false" aria-controls="collapseLayouts">
+                <a class="nav-link collapsed {{Request::is('admin/add-posts') || Request::is('admin/posts') ? 'show':'collapse'}}" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                     Posts
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePosts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="{{Request::is('admin/add-posts') || Request::is('admin/posts') ? 'show':'collapse'}}" id="collapsePosts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{url('admin/add-posts')}}">Add Posts</a>
-                        <a class="nav-link" href="{{url('admin/posts')}}">View Posts</a>
+                        <a class="nav-link {{Request::is('admin/add-posts')?"active":''}}" href="{{url('admin/add-posts')}}">Add Posts</a>
+                        <a class="nav-link {{Request::is('admin/posts')?"active":''}}" href="{{url('admin/posts')}}">View Posts</a>
                     </nav>
                 </div>
-                <a class="nav-link" href="{{url('admin/users')}}">
+                <a class="nav-link {{Request::is('admin/users')?'active':''}}" href="{{url('admin/users')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Users
                 </a>
